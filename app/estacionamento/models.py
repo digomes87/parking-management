cbc87df17d585c03e3738fb0a08ffc30da8494fe
Diego G <diego.gomes87@gmail.com>
2020-03-26 14:32:58 -0300

    done classe models

--- app/estacionamento/models.py
+++ app/estacionamento/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import math
+import decimal
 
 
 class Pessoa(models.Model):
@@ -34,3 +36,41 @@ class Paramentros(models.Model):
 
     def __str__(self):
         return 'Paramentros Gerais'
+
+
+class MovRotativo(models.Model):
+    checkin = models.DateTimeField(auto_now=False)
+    checkout = models.DateTimeField(auto_now=False, null=True, blank=True)
+    valor_hora = models.DecimalField(max_digits=5, decimal_places=2)
+    veiculo = models.ForeignKey(Veiculo, on_delete=models.CASCADE)
+    pago = models.BooleanField(default=False)
+
+    def horas_total(self):
+        if self.checkout is None:
+            print("Erro na data")
+        else:
+            return math.ceil((self.checkout - self.checkin).total_seconds() / 3600)
+
+    def total(self):
+        return self.valor_hora * decimal.Decimal(self.horas_total())
+
+    def __str__(self):
+        return self.veiculo.placa
+
+
+class Mensalista(models.Model):
+    veiculo = models.ForeignKey(Veiculo, on_delete=models.CASCADE)
+    inicio = models.DateField()
+    valor_mes = models.ForeignKey(Paramentros, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.veiculo) + '-' + str(self.inicio)
+
+
+class MovMensalista(models.Model):
+    mensalista = models.ForeignKey(Mensalista, on_delete=models.CASCADE)
+    dt_pgto = models.DateField()
+    total = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return str(self.mensalista) + ' - ' + str(self.total)
