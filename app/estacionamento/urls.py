15dafa0de223153371cc77c7b14a9df67e8ddbe0
Diego G <diego.gomes87@gmail.com>
2020-04-01 09:36:23 -0300

    add form

--- app/estacionamento/urls.py
+++ app/estacionamento/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path, include
-from .views import home, lista_pessoa
+from django.urls import path
+from .views import home, lista_pessoa, lista_veiculo, lista_movimento, pessoa_novo
+
 urlpatterns = [
     path('', home, name='home'),
     path('pessoas/', lista_pessoa, name='lista_pessoa'),
+    path('pessoas-novo/', pessoa_novo, name='pessoa_novo'),
+    path('veiculos/', lista_veiculo, name='lista_veiculo'),
+    path('movimentos/', lista_movimento),
 
 ]
--- app/estacionamento/views.py
+++ app/estacionamento/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from .models import Pessoa
+from django.shortcuts import render, redirect
+from .models import Pessoa, Veiculo, MovRotativo
+from .form import PessoaForm
+
 
 def home(request):
     context = {'mensagem': 'aqui uma msg'}
@@ -8,4 +10,23 @@ def home(request):
 
 def lista_pessoa(request):
     pessoas = Pessoa.objects.all()
-    return render(request, 'estacionamento/lista_pessoas.html', {'pessoas':pessoas})
+    form = PessoaForm()
+    data = {'pessoas': pessoas, 'form': form}
+    return render(request, 'estacionamento/lista_pessoas.html', data)
+
+
+def pessoa_novo(request):
+    form = PessoaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    return redirect('lista_pessoa')
+
+
+def lista_veiculo(request):
+    veiculo = Veiculo.objects.all()
+    return render(request, 'estacionamento/lista_veiculo.html', {'veiculo': veiculo})
+
+
+def lista_movimento(request):
+    lista_movimento = MovRotativo.objects.all()
+    return render(request, 'estacionamento/lista_movimento.html', {'lista_movimento': lista_movimento})
